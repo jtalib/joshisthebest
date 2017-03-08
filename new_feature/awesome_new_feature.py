b0b3867e2fe55f6fea638719c47b05d4efbfaa40
cdunning <c.m.dunning@pgr.reading.ac.uk>
2017-03-08 12:42:18 +0000

    I changed the awesome python script

--- new_feature/awesome_new_feature.py
+++ new_feature/awesome_new_feature.py
@@ -1,2 +1,4 @@
 if __name__ == '__main__':
     print('Everything is awesome!!!')
+    print('Everything is cool when you're part of the team!')
+    print('I'm not sure everything is awesomwe when you're using Python3')
